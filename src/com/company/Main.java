a4d7465f4978eede794aa1826766911e6197bcdd
User <janylkalykovna>
2022-02-15 14:26:38 +0600

    Массивдин тапшырмасы

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = new int[10];
+     int[] aray={1,2,3,4,5,6,7,9,10};
+       for (int i = 0; i < array.length; i=i+1) {
+           array[i]=i+1;
+            System.out.println(array[i]);
+
+        /*int[] array1 = new int[10];
+        for (int j = 0; j < array1.length; j++) {
+            array1[j] = j + 1;
+            if (array1[j] % 2 == 0) {
+                System.out.println(array1[j]);*/
+            }
+
+        }
+
+    }
+
+
